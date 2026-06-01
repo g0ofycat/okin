e92c401f0818afbaab33328ff48cbd6cdf59a6f1
g0ofycat <clintonusa5870@gmail.com>
2026-06-01 19:25:30 -0400

    [FIX]: cmake arena location; [ADD]: some stuff to compiler struct before writing compiler

--- src/compiler/compiler.h
+++ src/compiler/compiler.h
@@ -7,14 +7,28 @@
 #include "scope.h"
 
 // ======================
+// -- CONSTS
+// ======================
+
+#define MAX_FUNCTIONS 256
+
+// ======================
 // -- CLASSES & STRUCTS
 // ======================
+
+typedef struct {
+	const char *name;
+	size_t      name_len;
+	int         index;
+} func_entry_t;
 
 typedef struct {
 	const parser_t *parser;
 	chunk_t        *root;
 	chunk_t        *current_scope;
 	scope_t        *scope;
+	func_entry_t    functions[MAX_FUNCTIONS];
+	int             func_count;
 
 	int errors;
 } compiler_t;
